c0f5d497e0ff18b98c7f95832826cc89d54fb7ef
dre36dre <dre.son36@gmail.com>
2024-11-02 09:51:45 -0300

    Construção da ApiRest.

--- servidorpublicobdwebRESTApi/src/main/java/com/dre/servidorpublico/servidorpublicobdwebrest/api/ServidorPublicoAPIRest.java
+++ servidorpublicobdwebRESTApi/src/main/java/com/dre/servidorpublico/servidorpublicobdwebrest/api/ServidorPublicoAPIRest.java
@@ -1,14 +1,13 @@
 package com.dre.servidorpublico.servidorpublicobdwebrest.api;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.dre.servidorpublico.servidorpublicobdwebrest.entity.ServidorPublico;
 
@@ -16,64 +15,18 @@ public interface ServidorPublicoAPIRest {
 
 	@GetMapping("/listarServidores")
 	public ResponseEntity<List<ServidorPublico>> listarServidores();
-	{
-		model.addAttribute("servidorespublicos",servidorService.listAll());
-		return "servidorespublicos";
-	}
 
 	@GetMapping("/listarServidor/{matricula}")
-	public String listarServidor(@PathVariable long matricula, Model model)
-	{
-		model.addAttribute("servidorpublico",servidorService.listByMatricula(matricula).get());
-		return "servidorpublico";
-	}
-	
+	public String listarServidor(@PathVariable long matricula);
+
 	@GetMapping("/excluirServidor/{matricula}")
-	public String excluirServidor(@PathVariable long matricula)
-	{
-		servidorService.delete(matricula);
-		return "redirect:/listarServidores";
-	}
+	public void excluirServidor(@PathVariable long matricula);
 	
-	@GetMapping("/formularioEditarServidor/{matricula}")
-	public String formEditarServidor(@PathVariable long matricula, Model model)
-	{
-		Optional<ServidorPublico> servidorEncontrado = servidorService.listByMatricula(matricula);
-		model.addAttribute("servidorPublico",servidorEncontrado);
-		return "editarservidorpublico";
-	}
+	@PutMapping("/editarServidor/{matricula}")
+	public String editarServidor(@PathVariable long matricula, @RequestBody  ServidorPublico servidor);
 	
-	@PostMapping("/editarServidor/{matricula}")
-	public String editarServidor(@ModelAttribute ServidorPublico servidor)
-	{
-		servidorService.update(servidor);
-		return "redirect:/listarServidores";
-	}
-	//Novo servidor
-	@GetMapping("/formularioNovoServidor")
-	public String formNovoServidor(Model model)
-	{
-		model.addAttribute("servidorPublico",new ServidorPublico());
-		return "novoservidorpublico";
-	}
-	
+
 	@PostMapping("/cadastrarServidor")
-	public String cadastrarServidor(@ModelAttribute ServidorPublico novoservidor)
-	{
-		Optional<ServidorPublico> servidorEncontrado = 
-				servidorService.listByMatricula(novoservidor.getMatricula());
-		
-		if (!servidorEncontrado.isPresent())
-			servidorService.save(novoservidor);
-		else
-			return "redirect:/mensagem";
-		return "redirect:/listarServidores";
-	}
+	public String cadastrarServidor(@RequestBody ServidorPublico novoservidor);
 	
-	@GetMapping("/mensagem")
-	public String mensagem(Model model)
-	{
-		model.addAttribute("erroMatriculaExistente",true);
-		return "erro/mensagem";
-	}
 }
